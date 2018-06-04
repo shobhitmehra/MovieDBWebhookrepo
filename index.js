a00155279c54217c5c6bab3b0da1be2090c61603
Shobhit Mehra <shobhitmehra1@gmail.com>
2018-06-04 12:39:56 +0530

    Update index.js

--- index.js
+++ index.js
@@ -27,7 +27,7 @@ server.post('/get-movie-details', (req, res) => {
             var movie = JSON.parse(completeResponse);
 		
            let dataToSend = movieToSearch === 'The Godfather' ? `I don't have the required info on that. Here's some info on 'The Godfather' instead.\n` : '';
-            dataToSend += `${movie.Title} is a ${movie.Actors} starer ${movie.Genre} movie, released in ${movie.Year}. It was directed by ${movie.Director}`;
+            dataToSend += `${movie.Title} is a ${movie.Actors} starer ${movie.Genre} movie, released in ${movie.Year}. It was directed by ${movie.Director}\n\n: abcd`;
   		
 
             return res.json({
@@ -42,7 +42,7 @@ server.post('/get-movie-details', (req, res) => {
             displayText: 'Something went wrong!',
             source: 'get-movie-details'
   
-}
+
 		
 	   
         });
