de933d5aa82007131111cb5df052543f3ae2a8d4
Shobhit Mehra <shobhitmehra1@gmail.com>
2018-06-07 12:00:10 +0530

    Update index.js

--- index.js
+++ index.js
@@ -28,13 +28,13 @@ server.post('/get-movie-details', (req, res) => {
 		
            let dataToSend = movieToSearch === 'The Godfather' ? `I don't have the required info on that. Here's some info on 'The Godfather' instead.\n` : '';
           
-		/*if(${movie.response}=="False"){
+		if(${movie.Response}=="False"){
 			dataToSend='could not find the movie';
 		}
 		else{
-		*/
-		dataToSend += `${movie.Title} is a ${movie.Actors} starer ${movie.Genre} movie, released in ${movie.Year}. It was directed by ${movie.Director}.  \r\n \r\n If you need information about another movie, you can ask me. `;
-		/*}*/
+		
+		dataToSend = `${movie.Title} is a ${movie.Actors} starer ${movie.Genre} movie, released in ${movie.Year}. It was directed by ${movie.Director}.  \r\n \r\n If you need information about another movie, you can ask me. `;
+		}
 
             return res.json({
                 speech: dataToSend,
