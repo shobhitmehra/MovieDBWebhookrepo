900cd4a8898330cc22c51058a17bd0d5a103ffea
Shobhit Mehra <shobhitmehra1@gmail.com>
2018-06-07 11:51:12 +0530

    Update index.js

--- index.js
+++ index.js
@@ -27,8 +27,14 @@ server.post('/get-movie-details', (req, res) => {
             var   movie = JSON.parse(completeResponse);
 		
            let dataToSend = movieToSearch === 'The Godfather' ? `I don't have the required info on that. Here's some info on 'The Godfather' instead.\n` : '';
-            dataToSend += `${movie.Title} is a ${movie.Actors} starer ${movie.Genre} movie, released in ${movie.Year}. It was directed by ${movie.Director}.  \r\n \r\n If you need information about another movie, you can ask me. `;
-  		
+          
+		if(movie.response==False){
+			dataToSend='could not find the movie';
+		}
+		else{
+		
+		dataToSend += `${movie.Title} is a ${movie.Actors} starer ${movie.Genre} movie, released in ${movie.Year}. It was directed by ${movie.Director}.  \r\n \r\n If you need information about another movie, you can ask me. `;
+		}
 
             return res.json({
                 speech: dataToSend,
